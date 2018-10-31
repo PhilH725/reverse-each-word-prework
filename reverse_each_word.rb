6d29463a6fdde5dd97deaf8acb6049ebd2b296a3
Phil Hartley <philh725@gmail.com>
2018-10-31 23:52:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   temp = sentence.split(' ')
-  test = temp.collect do |word|
+  testing = temp.collect do |word|
     word.reverse
   end
-  test.join(' ')
+  testing.join(' ')
 end
